5f78fddd376ff3aab7693f7cd561b8db36ebe7f4
Tormund Haus <tormundhaus@gmail.com>
2012-04-08 00:35:18 +0200

    Added Driver and TestDriver

---
+++ recs/src/edu/ntnu/ttk4145/recs/driver/TestDriver.java
@@ -0,0 +1,44 @@
+package edu.ntnu.ttk4145.recs.driver;
+
+import java.io.IOException;
+
+public class TestDriver extends Driver{
+
+	@Override
+	protected void buttonPressed(Button button, int floor) {
+		setButtonLamp(button,floor,true);
+	}
+	
+	@Override
+	protected void stopButtonPressed() {
+		setStopLamp(true);
+		System.out.println("Stop button pushed");
+	}
+	
+	@Override
+	protected void floorSensorTriggered(int floor, boolean arriving) {
+		setFloorIndicator(floor);
+		
+		if(floor == 0){
+			setSpeed(1000);
+		} else if (floor == 3){
+			setSpeed(-1000);
+		}
+		System.out.printf("Floor %d, %s%n",floor + 1,arriving ? "arrive" : "depart");
+	}
+
+	@Override
+	protected void obstructionSensorTriggered(boolean enabled) {
+		setDoorOpenLamp(enabled);
+		System.out.println("Obstruction switch flipped");
+	}
+	
+	public static void main(String[] args) {
+		Driver driver = Driver.makeInstance(TestDriver.class);
+		
+		System.out.println("Started");
+		try {
+			System.in.read();
+		} catch (IOException e) {}
+	}
+}
