7ad166d21fdcc9a9f298fe1087606086e61a7ad7
Lars <mrgrandmother@gmail.com>
2012-04-13 20:27:32 +0200

    +getId() to compare peer Ids to determine master.
    +Peer(InetAddress address, long id)

--- recs/src/edu/ntnu/ttk4145/recs/Peer.java
+++ recs/src/edu/ntnu/ttk4145/recs/Peer.java
@@ -1,18 +1,39 @@
 package edu.ntnu.ttk4145.recs;
 
+import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
+
+import edu.ntnu.ttk4145.recs.manager.Manager;
 
 public class Peer {
 
 	
 	private InetAddress ip;
-	private long timestamp;
+	private long timeOfLastAlive;
+	private long id;
+	private DatagramSocket socket = null;
 	
-	
-	public void sendMessage(Message msg){
-		
+	public Peer(InetAddress address, long id) {
+		ip = address;
+		this.id = id;
+		try {
+			socket = new DatagramSocket(Manager.getSendport());
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	
+	public boolean sendMessage(Message msg){
+		return true;
+	}
 	
+	public void setTimeOfLastAlive(long timeOfLastAlive) {
+		this.timeOfLastAlive = timeOfLastAlive;	
+	}
+	
+	public long getId() {
+		return id;
+	}
 }
